867f6f4ef989c5056cdacd8757058f9965d50d24
P4R4DiSi4C <paradisiac.web@hotmail.ch>
2016-08-27 23:15:53 +0200

    Made 2 new classes

---
+++ SRC-P4R4-PokeMob-Creator/P4R4-PokeMob-Creator/Classes/Proxies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4R4_PokeMob_Creator.Classes
+{
+    class Proxies
+    {
+    }
+}
