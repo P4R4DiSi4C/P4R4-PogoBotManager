278eba0f0ef6bb34f8e2583256f1ddfe4372db95
David Carvalho <paradisiac.web@hotmail.ch>
2016-08-31 10:12:04 +0200

    Update Logger.cs

--- SRC-P4R4-PokeMob-Creator/P4R4-PokeMob-Creator/Classes/Logger.cs
+++ SRC-P4R4-PokeMob-Creator/P4R4-PokeMob-Creator/Classes/Logger.cs
@@ -1,9 +1,5 @@
-﻿
-<<<<<<< HEAD
-using System;
-=======
+
 using System.Windows.Forms;
->>>>>>> refs/remotes/origin/master
 
 namespace P4R4_PokeMob_Creator
 {
@@ -27,19 +23,7 @@ namespace P4R4_PokeMob_Creator
         /// <param name="logText"></param>
         public void AppendLog(string logText)
         {
-<<<<<<< HEAD
-            if (_mainForm._creationLogsRichTxtBox.InvokeRequired)
-            {
-                _mainForm._creationLogsRichTxtBox.BeginInvoke(new Action(delegate {
-                    AppendLog(logText);
-                }));
-                return;
-            }
-
-            _mainForm._creationLogsRichTxtBox.AppendText(logText + "\n");
-=======
             _mainForm.CreationLogsActions(logText + "\n", false);
->>>>>>> refs/remotes/origin/master
         }
 
         /// <summary>
@@ -47,7 +31,7 @@ namespace P4R4_PokeMob_Creator
         /// </summary>
         public void ClearLogs()
         {
-            _mainForm._creationLogsRichTxtBox.Clear();
+            _mainForm.CreationLogsActions("", true);
         }
     }
 }
